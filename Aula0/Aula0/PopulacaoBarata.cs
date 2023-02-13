cc144af51487f0a95a610c640b1badb1c90fccab
Luchaos, Artificer of Ilha das Flores <89659834+marsh090@users.noreply.github.com>
2023-02-13 10:24:22 -0300

    Add files via upload
    codigo da aula

---
+++ Aula0/Aula0/PopulacaoBarata.cs
@@ -0,0 +1,30 @@
+﻿namespace Aula0
+{
+    internal class PopulacaoBarata
+    {
+
+        public int QtdeBaratas = 0;
+
+        public PopulacaoBarata()
+        {
+            this.QtdeBaratas = 100;
+        }
+
+        public int GetQtdeBaratas()
+        {
+            return this.QtdeBaratas;
+        }
+
+        public void Spray()
+        {
+            this.QtdeBaratas = (int)(this.QtdeBaratas * 0.9);
+        }
+
+        public void Procria()
+        {
+            this.QtdeBaratas = (int)(this.QtdeBaratas * 1.5);
+        }
+
+
+    }
+}
---
+++ Aula0/Aula0/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PopulacaoBarata baratas = new PopulacaoBarata();
+
+            Console.WriteLine("\nA população de baratas é:\n");
+            Console.WriteLine(baratas.GetQtdeBaratas());
+
+            baratas.Spray();
+            baratas.Spray();
+
+            Console.WriteLine("\nA população de baratas é:\n");
+            Console.WriteLine(baratas.GetQtdeBaratas());
+
+            baratas.Procria();
+
+            Console.WriteLine("\nA população de baratas é:\n");
+            Console.WriteLine(baratas.GetQtdeBaratas());
+
+
+        }
+    }
+}
