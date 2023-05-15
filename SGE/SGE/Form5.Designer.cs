f0db043804288e0f933eaebdb6e8046243e2658c
Luchaos, Artificer of Ilha das Flores <89659834+marsh090@users.noreply.github.com>
2023-05-15 10:46:10 -0300

    Adicionados arquivos referentes à atividade 4 SGE

---
+++ SGE/SGE/Form5.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace SGE
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtQtd = new System.Windows.Forms.TextBox();
+            this.lblQtd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtDesc.Location = new System.Drawing.Point(146, 181);
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(207, 23);
+            this.txtDesc.TabIndex = 23;
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblDesc.Location = new System.Drawing.Point(76, 182);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(58, 24);
+            this.lblDesc.TabIndex = 22;
+            this.lblDesc.Text = "Desc:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtNome.Location = new System.Drawing.Point(146, 140);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(207, 23);
+            this.txtNome.TabIndex = 21;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.AutoSize = true;
+            this.btnSalvar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnSalvar.Location = new System.Drawing.Point(193, 270);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(63, 30);
+            this.btnSalvar.TabIndex = 20;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtId.Location = new System.Drawing.Point(146, 101);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(207, 23);
+            this.txtId.TabIndex = 19;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblNome.Location = new System.Drawing.Point(76, 141);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(67, 24);
+            this.lblNome.TabIndex = 18;
+            this.lblNome.Text = "Nome:";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblId.Location = new System.Drawing.Point(76, 98);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(32, 24);
+            this.lblId.TabIndex = 17;
+            this.lblId.Text = "ID:";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lblTitulo.Location = new System.Drawing.Point(124, 43);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(180, 31);
+            this.lblTitulo.TabIndex = 16;
+            this.lblTitulo.Text = "Novo Produto";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtQtd
+            // 
+            this.txtQtd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtQtd.Location = new System.Drawing.Point(146, 224);
+            this.txtQtd.Name = "txtQtd";
+            this.txtQtd.Size = new System.Drawing.Size(207, 23);
+            this.txtQtd.TabIndex = 25;
+            // 
+            // lblQtd
+            // 
+            this.lblQtd.AutoSize = true;
+            this.lblQtd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblQtd.Location = new System.Drawing.Point(76, 225);
+            this.lblQtd.Name = "lblQtd";
+            this.lblQtd.Size = new System.Drawing.Size(45, 24);
+            this.lblQtd.TabIndex = 24;
+            this.lblQtd.Text = "Qtd:";
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 337);
+            this.Controls.Add(this.txtQtd);
+            this.Controls.Add(this.lblQtd);
+            this.Controls.Add(this.txtDesc);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "Form5";
+            this.Text = "Form5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDesc;
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtQtd;
+        private System.Windows.Forms.Label lblQtd;
+    }
+}
---
+++ SGE/SGE/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGE
+{
+    public class Usuario
+    {
+        public string Login { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public bool Habilitado { get; set; }
+
+        public Usuario(string login, string senha, bool habilitado)
+        {
+            Login = login;
+            Nome = senha;
+            Senha = senha;
+            Habilitado = habilitado;
+        }
+    }
+}
